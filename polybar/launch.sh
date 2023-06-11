cce269e84d8f3ff4e40f60da19030278c1016eaa
Callumjrowbi <c.rowbotom@outlook.com>
2023-06-11 20:41:50 +0100

    Some *Minor* changes. Tidied the config files up

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,7 +4,7 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar sysinfo 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar workspace 2>&1 | tee -a /tmp/polybar2.log & disown
 polybar window 2>&1 | tee -a /tmp/polybar3.log & disown
-polybar datetime 2>&1 | tee -a /tmp/polybar4.log & disown
+polybar extra 2>&1 | tee -a /tmp/polybar4.log & disown
   
 
 
