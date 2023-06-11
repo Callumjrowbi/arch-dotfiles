6781fa12db401251b88713147b49b11572190e80
Callumjrowbi <c.rowbotom@outlook.com>
2023-06-11 18:24:53 +0100

    More polybar changes (date)

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,20 +1,12 @@
-#!/usr/bin/env bash
+killall -q polybar
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
-
-# Launch bars
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar sysinfo 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar workspace 2>&1 | tee -a /tmp/polybar2.log & disown
 polybar window 2>&1 | tee -a /tmp/polybar3.log & disown
-polybar root 2>&1 | tee -a /tmp/polybar4.log & disown
+polybar datetime 2>&1 | tee -a /tmp/polybar4.log & disown
   
 
 
-echo "Bars launched..."
-
+echo "Polybar launched"
 
